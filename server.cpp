86d852b2c49e8272f4dbb60396e39bfa479ae12f
kurtlike <gordonfriman2000@gmail.com>
2023-12-09 21:06:20 +0300

    classic

--- server.cpp
+++ server.cpp
@@ -85,13 +85,8 @@ int main(int argc, char *argv[]) {
   }
 }
 
-<<<<<<< HEAD
 //haha
 int test(){
-    retutn 12;
-=======
-//new change
-int test(){
-    retutn 1;
->>>>>>> testing
+    retutn 8;
+
 }
